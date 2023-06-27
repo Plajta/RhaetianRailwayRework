fe29904c71073673e8d7783e3f7e2ae45c725919
HelloWorld7894 <danpojhan@protonmail.com>
2023-06-27 23:01:06 +0200

    form gui is done

--- src/code/index.js
+++ src/code/index.js
@@ -7,6 +7,11 @@ var All_people = ['65 years or more<button onclick="AddToIndex(0)">+</button>0<b
 'Child 0-5 years<button onclick="AddToIndex(6)">+</button>0<button onclick="DecreaseToIndex(6)">-</button>',
 "Confirm"]
 
+var destination_switch = false
+var depart = ""
+var arrival = ""
+var class_select = ""
+
 function Register(assign_switch){
     if (assign_switch){
         document.getElementsByClassName("dropdown-content")[0].style["display"] = "block"
@@ -16,6 +21,16 @@ function Register(assign_switch){
         assign_people = false
         document.getElementsByClassName("dropdown-content")[0].style["display"] = "none"
     }
+}
+
+function OpenClassMenu(){
+    document.getElementsByClassName("dropdown-content")[1].style["display"] = "block"
+}
+
+function CloseClassMenu(index){
+    class_select = (index + 1).toString()
+    document.getElementsByClassName("dropdown-content")[1].style["display"] = "none"
+    document.getElementById("ClassChange").innerText = class_select + ". class"
 }
 
 function DecreaseToIndex(index){
@@ -50,4 +65,39 @@ function refresh(){
         console.log(all_selections)
         all_selections[i].innerHTML = All_people[i]
     }
+}
+
+function shift_time(left_or_right){
+    var time_elem = document.getElementById("timeshow")
+    var time_str = time_elem.innerText
+    var time_main = time_str.substring(0, 2)
+    var new_time;
+    if (left_or_right){
+        //left
+        new_time = parseInt(time_main) - 1
+    }
+    else{
+        //right
+        new_time = parseInt(time_main) + 1
+    }
+    time_elem.innerText = new_time.toString() + ":00"
+}
+
+function switch_destinations(){
+    depart = document.getElementsByClassName("from")[0].value
+    arrival = document.getElementsByClassName("to")[0].value
+
+    if(destination_switch){
+        //not default
+        document.getElementById("arrowbutton").style["transform"] = ""
+        destination_switch = false
+    }
+    else{
+        //default
+        document.getElementById("arrowbutton").style["transform"] = "scaleX(-1)"
+        destination_switch = true
+    }
+
+    document.getElementsByClassName("from")[0].value = arrival
+    document.getElementsByClassName("to")[0].value = depart
 }

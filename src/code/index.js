82b2d89eecfe07db1df554c2bd36e8659e4df716
HelloWorld7894 <danpojhan@protonmail.com>
2023-06-28 02:33:35 +0200

    Passanger adding works

--- src/code/index.js
+++ src/code/index.js
@@ -1,10 +1,10 @@
-var All_people = ['65 years or more<button onclick="AddToIndex(0)">+</button>0<button onclick="DecreaseToIndex(0)">-</button>', 
-'Adult 26-64 years<button onclick="AddToIndex(1)">+</button>0<button onclick="DecreaseToIndex(1)">-</button>', 
-'Junior 18-25 years<button onclick="AddToIndex(2)">+</button>0<button onclick="DecreaseToIndex(2)">-</button>', 
-'Junior 15-17 years<button onclick="AddToIndex(3)">+</button>0<button onclick="DecreaseToIndex(3)">-</button>', 
-'Child 10-14 years<button onclick="AddToIndex(4)">+</button>0<button onclick="DecreaseToIndex(4)">-</button>', 
-'Child 6-9 years<button onclick="AddToIndex(5)">+</button>0<button onclick="DecreaseToIndex(5)">-</button>', 
-'Child 0-5 years<button onclick="AddToIndex(6)">+</button>0<button onclick="DecreaseToIndex(6)">-</button>',
+var All_people = ['65 years and more<button onclick="AddToIndex(0)" id="inline-button">+</button>0<button onclick="DecreaseToIndex(0)" id="inline-button">-</button>', 
+'Adult 26-64 years<button onclick="AddToIndex(1)" id="inline-button">+</button>0<button onclick="DecreaseToIndex(1)" id="inline-button">-</button>', 
+'Junior 18-25 years<button onclick="AddToIndex(2)" id="inline-button">+</button>0<button onclick="DecreaseToIndex(2)" id="inline-button">-</button>', 
+'Junior 15-17 years<button onclick="AddToIndex(3)" id="inline-button">+</button>0<button onclick="DecreaseToIndex(3)" id="inline-button">-</button>', 
+'Child 10-14 years<button onclick="AddToIndex(4)" id="inline-button">+</button>0<button onclick="DecreaseToIndex(4)" id="inline-button">-</button>', 
+'Child 6-9 years<button onclick="AddToIndex(5)" id="inline-button">+</button>0<button onclick="DecreaseToIndex(5)" id="inline-button">-</button>', 
+'Child 0-5 years<button onclick="AddToIndex(6)" id="inline-button">+</button>0<button onclick="DecreaseToIndex(6)" id="inline-button">-</button>',
 "Confirm"]
 
 var destination_switch = false
@@ -13,6 +13,7 @@ var arrival = ""
 var class_select = ""
 
 function Register(assign_switch){
+    //gui
     if (assign_switch){
         document.getElementsByClassName("dropdown-content")[0].style["display"] = "block"
         assign_people = true
@@ -20,6 +21,26 @@ function Register(assign_switch){
     else{
         assign_people = false
         document.getElementsByClassName("dropdown-content")[0].style["display"] = "none"
+        
+        var string_result = "";
+
+        //confirm and update
+        for(let i = 0; i < All_people.length; i++){
+            let idx = All_people[i].indexOf("+</button>") + 10
+            let num = parseInt(All_people[i][idx])
+
+            if (num > 0){
+                let section = All_people[i].substring(0, All_people[i].indexOf("<button"))
+                string_result += num.toString() + "x " + section + "\n"
+            }
+        }
+
+        if (string_result.length == 0){
+            document.getElementById("dropbtn-text").innerText = "> Select passangers"
+        }
+        else{
+            document.getElementById("dropbtn-text").innerText = string_result
+        }
     }
 }
 
@@ -62,7 +83,6 @@ function AddToIndex(index){
 function refresh(){
     var all_selections = document.getElementsByClassName("dropdown-content")[0].children
     for(let i = 0; i < all_selections.length; i++){
-        console.log(all_selections)
         all_selections[i].innerHTML = All_people[i]
     }
 }
@@ -114,3 +134,4 @@ function GoBack(){
 function Buy(){
     window.location.href = "./process.html"
 }
+
